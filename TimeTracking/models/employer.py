063cd381e73de1d51ebfbf11e500601a07090408
YNCK-YNCK <info@yannick-bell.de>
2025-07-18 22:44:44 +0200

    Update employer.py

--- TimeTracking/models/employer.py
+++ TimeTracking/models/employer.py
@@ -7,7 +7,7 @@ class Employer:
     def add_employee(self, employee):
         self.employees[employee.employee_id] = employee
 
-    def remove_employee(self, employee_id):
+    def remove_employee(self, employee_id): # Note: currently not implemented.
         if employee_id in self.employees:
             del self.employees[employee_id]
 
